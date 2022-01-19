d5c605a082eb7a62bb25e0fc2fd3cd4e2d7f7a2a
unknown <diwserala@gmail.com>
2022-01-19 15:48:25 +0545

    Initial commit

---
+++ src/main/java/com/example/reddit_clone/reddit_clone_back/RedditCloneBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.reddit_clone.reddit_clone_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditCloneBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedditCloneBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reddit_clone/reddit_clone_back/RedditCloneBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reddit_clone.reddit_clone_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedditCloneBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
